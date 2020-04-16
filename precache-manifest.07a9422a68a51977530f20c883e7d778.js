3d38d983bf1375f95f933edfbea0facd1e70ec00
Leah <LeahKahwadjian@gmail.com>
2020-04-16 12:21:01 -0400

    Updates

---
+++ precache-manifest.07a9422a68a51977530f20c883e7d778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e62096731416b6b96e0896cb8df370",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d7fc53ee357e84996b09",
+    "url": "/ToDoList/static/css/main.f81e7af6.chunk.css"
+  },
+  {
+    "revision": "5f8f36d96cb3de520593",
+    "url": "/ToDoList/static/js/2.6108bf5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.6108bf5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fc53ee357e84996b09",
+    "url": "/ToDoList/static/js/main.27e16b7a.chunk.js"
+  },
+  {
+    "revision": "894e80690669648fd0df",
+    "url": "/ToDoList/static/js/runtime-main.c148c603.js"
+  }
+]);
